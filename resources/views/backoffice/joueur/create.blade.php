fbd0a469b823db2179ced92275d27a787460923e
Abdel <elb.abd@icloud.com>
2021-05-27 09:52:06 +0200

    edit de joueur

--- resources/views/backoffice/joueur/create.blade.php
+++ resources/views/backoffice/joueur/create.blade.php
@@ -22,12 +22,36 @@
                 <input type="number" class="form-control"  name="age">
             </div>
             <div class="mb-3">
-                <label class="form-label">email</label>
-                <input type="text" class="form-control"  name="email">
+                <label class="form-label">numero</label>
+                 <input type="number" class="form-control" name="numero">
             </div>
             <div class="mb-3">
-                <label class="form-label">Mot de passe</label>
-                <input type="password" class="form-control"  name="mdp">
+                <label class="form-label">Pays</label>
+                <input type="text" class="form-control" name="pays">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Genre</label>
+                <select name="genre_id" class="form-control" >
+                    @foreach ($genres as $genre)
+                        <option value="{{ $genre->id }}">{{ $genre->type }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Role</label>
+                <select name="role_id" class="form-control" >
+                    @foreach ($roles as $role)
+                        <option value="{{ $role->id }}">{{ $role->nom }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Équipe</label>
+                <select name="equipe_id" class="form-control" >
+                    @foreach ($equipes as $equipe)
+                        <option value="{{ $equipe->id }}">{{ $equipe->nom }}</option>
+                    @endforeach
+                </select>
             </div>
             <div class="mb-3">
               <label class="form-label">Photo de profil</label>
